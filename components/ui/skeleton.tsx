451ef03ef7355748a803a4793545b432dcebc4f3
Oliver <oliver.lewandowski1@gmail.com>
2023-07-07 19:50:03 +0200

    loaders added

---
+++ components/ui/skeleton.tsx
@@ -0,0 +1,12 @@
+import { cn } from "@/lib/utils";
+
+const Skeleton = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn("animate-pulse rounded-md bg-neutral-200", className)}
+    {...props}
+  />
+);
+export default Skeleton;
